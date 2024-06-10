93e1916ace45c8ded9aaf8134cdbcd23996dab9c
Luke McMeans <elukemcmeans@gmail.com>
2024-06-09 21:37:55 -0400

    Added background image

--- app/navbar.js
+++ app/navbar.js
@@ -5,13 +5,19 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-800 p-4">
       <Image 
-        src='/images/McMeansLogo_transparent.png'
-        width={60}
-        height={60}
-      />
-      <div className="centered-items">
-        <Link href="/" class="navbar-item">Home</Link>
-        <Link href="/resume" class="navbar-item">Resume</Link>
+          src='/images/McMeansLogo_transparent.png'
+          width={60}
+          height={60}
+        />
+      <div className="flex items-center justify-between">
+        <div>
+          <Link href="/" passHref>
+            <span className="navbar-item">Home</span>
+          </Link>
+          <Link href="/resume" passHref>
+            <span className="navbar-item">Resume</span>
+          </Link>
+        </div>
       </div>
     </nav>
   );
--- app/resume.tsx
+++ app/resume.tsx
@@ -1,1 +1,12 @@
 import Navbar from "./navbar.js";
+
+export default function Resume() {
+    return (
+      <div className="whole-page">
+          <Navbar />
+          <main className="content">
+              Resume
+          </main>
+      </div>
+    );
+  }
